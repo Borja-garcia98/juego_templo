c3aa42d8a575e693cc20a65b7c6344616ea3a16e
Borja <bgarciab2021@cepnet.net>
2021-11-29 13:13:19 +0100

    sql en prueba

--- mysql.js
+++ mysql.js
@@ -3,13 +3,10 @@
 const mysql = require ('mysql');
 
 const conection = mysql.createConnection({
-    host:'localhost',
-    user:'Borja',
-    password:'Borja',
+    host:'127.0.0.1',
+    user:'borja',
+    password:'borja',
     database: 'usuarios'
-
-
-
 })
 
 conection.connect(  (err) =>  {
@@ -18,3 +15,30 @@ conection.connect(  (err) =>  {
     console.log('la conexion funciona')
 
 })
+
+conection.query( 'SELECT * from users', (err, rows) => {
+
+    if(err) throw err 
+
+    console.log('los datos de la tabla son estos: ')
+    console.log(rows)
+
+    console.log('la cantidad de resultados es: ')
+    console.log (rows.lenght) // numero de resultados que devuekve 
+
+    
+    const usuarioUno = rows[0]
+   
+   console.log(`El usuario tiene id ${usuarioUno.id} y tiene puntuacion ${usuarioUno.puntuacion}` ) // con mensaje  
+//    console.log(usuarioUno.id) // capturar solo un usuario (sin mensaje como el de arriba) --> puedo acceder solo al dato que yo quiero ( id)
+
+    
+
+
+
+
+
+
+} )
+
+conection.end()

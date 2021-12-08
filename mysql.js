e4dcc0a9612c0347809c4d4448132339d795f563
Borja <bgarciab2021@cepnet.net>
2021-12-08 18:48:41 +0100

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -5,7 +5,7 @@ const mysql = require ('mysql');
 const conection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'YES',
+    password:'',
     database: 'usuarios'
 })
 
